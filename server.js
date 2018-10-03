f9a794d15905fd891df087a79bba230c78dec0a2
Ainsley Bernard <ains_bernard@hotmail.com>
2018-10-03 16:11:24 -0400

    connect to MangoDb database

--- server.js
+++ server.js
@@ -8,6 +8,17 @@ var app = express()
 //that will then share with express and sockey.io
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
+var mongoose = require('mongoose')
+var settings = require('./app_settings.js')
+var dbUrl = settings.dbUrl
+
+
+
+var Message = mongoose.model('Message', {
+    name: String,
+    message: String
+})
+
 
 app.use(express.static(__dirname))
 //this lets body parser know that we expect JSON to be coming in with our HTTP request.
@@ -15,21 +26,43 @@ app.use(bodyParser.json())
 //add this because what comes in from the browser is URL encoded, so we must setup body parser to support this.
 app.use(bodyParser.urlencoded({ extended: false }))
 
-var messages = [{ name: 'bob', message: 'hi' }, { name: 'sam', message: 'hi' }]
+
+
 
 app.get('/messages', (req, res) => {
-    res.send(messages)
+    Message.find({},(err, messages)=>{
+        res.send(messages)
+
+    })
 })
 
 app.post('/messages', (req, res) => {
-    messages.push(req.body)
-    io.emit('message',req.body)
-    res.sendStatus(200)
+
+    var message = new Message(req.body)
+    message.save((err => {
+        
+        if (err)     
+            sendStatus(500)
+
+
+        io.emit('message', req.body)
+        res.sendStatus(200)
+       
+
+        
+    }))
+
+
 })
 
 io.on('connection', (socket) => {
     console.log('user connected')
 })
+
+mongoose.connect(dbUrl, (err) => {
+    console.log('mongo db connection', err)
+})
+
 
 var server = http.listen(3000, () => {
     console.log('server is listening on port', server.address().port)
