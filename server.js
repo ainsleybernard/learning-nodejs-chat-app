d4322b1e2bbc2b933622360d9f4f93f6a04e0404
Ainsley Bernard <ains_bernard@hotmail.com>
2018-10-09 11:27:18 -0400

    add promise and async version examples of callback post message

--- server.js
+++ server.js
@@ -26,32 +26,102 @@ app.use(bodyParser.json())
 //add this because what comes in from the browser is URL encoded, so we must setup body parser to support this.
 app.use(bodyParser.urlencoded({ extended: false }))
 
-
+//indicate to mongoose which promise library we want to use, in this case the ES6 Promise library
+mongoose.Promise = Promise
 
 
 app.get('/messages', (req, res) => {
-    Message.find({},(err, messages)=>{
+    Message.find({}, (err, messages) => {
         res.send(messages)
 
     })
 })
 
-app.post('/messages', (req, res) => {
 
-    var message = new Message(req.body)
-    message.save((err => {
-        
-        if (err)     
-            sendStatus(500)
+// Example of nested callback hell
+// app.post('/messages', (req, res) => {
+
+//     var message = new Message(req.body)
+//     message.save((err => {
+
+//         if (err)
+//             sendStatus(500)
+
+//         Message.findOne({ message: 'badword' }, (err, censored) => {
+//             if (censored) {
+//                 console.log('censored words found', censored)
+//                 Message.remove({ _id: censored.id }, (err) => {
+//                     console.log('removed censored message')
+//                 })
+//             }
+//         })
+
+//         io.emit('message', req.body)
+//         res.sendStatus(200)
+//     }))
+// })
 
 
-        io.emit('message', req.body)
+//example using promises, another way to work with async code.
+//promises return an object which promise to do some work
+// this object has separate callbacks for success and for failures.
+// app.post('/messages', (req, res) => {
+
+//     var message = new Message(req.body)
+
+//     message.save()
+//         .then(() => {
+//             console.log('saved')
+//             return Message.findOne({ message: 'badword' })
+//         })
+//         .then(censored => {
+//             if (censored) {
+//                 console.log('censored words found', censored)
+//                 return Message.deleteOne({ _id: censored.id })
+//             }
+//             io.emit('message', req.body)
+//             res.sendStatus(200)
+
+//         })
+
+//         .catch((err) => {
+//             res.sendStatus(500)
+//             return console.error(err)
+//         })
+// })
+
+//async example
+//Makes asynchronous code look more synchronous
+//ti wirk with await, wee wil need to declare our express function as async
+ app.post('/messages', async (req, res) => {
+
+
+    try {
+
+        throw 'some error'
+        var message = new Message(req.body)
+
+        var savedMessage = await message.save()
+        console.log('saved')
+        var censored = await Message.findOne({ message: 'badword' })
+
+
+        if (censored) {
+            await Message.deleteOne({ _id: censored.id })
+            console.log('removed censored message')
+        }
+        else {
+            io.emit('message', req.body)
+
+        }
+
         res.sendStatus(200)
-       
-
-        
-    }))
-
+    } catch (error) {
+        res.sendStatus(500)
+        return console.error(error)
+    } finally {
+        console.log('message post called')
+    }
 
 })
 
